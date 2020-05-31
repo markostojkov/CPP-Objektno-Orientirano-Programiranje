d42fe7b57f71ed53b7e366649cea860292f63197
marko <marko@DESKTOP-DI9NG70>
2020-05-31 15:06:47 +0200

    Zadaca1

--- Lab9/Zadaca1.cpp
+++ Lab9/Zadaca1.cpp
@@ -8,253 +8,229 @@ using namespace std;
 
 class AlkoholPercentException
 {
-    protected:
-        char *message;
+public:
+    AlkoholPercentException()
+    {
+    }
 
-    public:
-        AlkoholPercentException(char *message)
-        {
-            this->message = new char[strlen(message) + 1];
-            strcpy(this->message, message);
-        }
+    void printMessage()
+    {
+        cout << "Pogresno vnesen procent na alkohol!" << endl;
+    }
 
-        void printMessage()
-        {
-            cout << this->message << endl;
-        }
-
-        ~AlkoholPercentException()
-        {
-            delete [] this->message;
-        }
 };
 
 
 class RegistrationException
 {
-    protected:
-        char *message;
+public:
+    RegistrationException()
+    {
+    }
 
-    public:
-        RegistrationException(char *message)
-        {
-            this->message = new char[strlen(message) + 1];
-            strcpy(this->message, message);
-        }
-
-        void printMessage()
-        {
-            cout << this->message << endl;
-        }
-
-        ~RegistrationException()
-        {
-            delete [] this->message;
-        }
+    void printMessage()
+    {
+        cout << "Pogresno vnesena registracija" << endl;
+    }
 };
 
 
 class ImaMasa
 {
-    public:
-        virtual double vratiMasa() = 0;
+public:
+    virtual double vratiMasa() = 0;
 
-        virtual void pecati() = 0;
+    virtual void pecati() = 0;
 };
 
 
 class PaketPijalok
 {
-    protected:
-        double volumenNaEdenPaket;
-        int kolicina;
-    
-    public:
-        static constexpr double masaNaAmbalaza = 0.2;
-        static constexpr double gustina = 0.8;
+protected:
+    double volumenNaEdenPaket;
+    int kolicina;
 
-        PaketPijalok(double volumenNaEdenPaket, int kolicina)
-        {
-            this->volumenNaEdenPaket = volumenNaEdenPaket;
-            this->kolicina = kolicina;
-        }
+public:
+    static const double masaNaAmbalaza = 0.2;
+    static const double gustina = 0.8;
 
-        double vratiMasa()
-        {
-            return (this->volumenNaEdenPaket * PaketPijalok::gustina + PaketPijalok::masaNaAmbalaza) * this->getKolicina();
-        }
+    PaketPijalok(double volumenNaEdenPaket, int kolicina)
+    {
+        this->volumenNaEdenPaket = volumenNaEdenPaket;
+        this->kolicina = kolicina;
+    }
 
-        void pecati()
-        {
-            cout << "Kolicina " << this->kolicina;
-        }
+    double vratiMasa()
+    {
+        return (this->volumenNaEdenPaket * PaketPijalok::gustina + PaketPijalok::masaNaAmbalaza) * this->getKolicina();
+    }
 
-        int getKolicina()
-        {
-            return this->kolicina;
-        }
+    void pecati()
+    {
+        cout << "Kolicina " << this->kolicina;
+    }
+
+    int getKolicina()
+    {
+        return this->kolicina;
+    }
 };
 
 
 class PaketSok : public PaketPijalok, public ImaMasa
 {
-    protected:
-        bool gaziran;
+protected:
+    bool gaziran;
 
-    public:
-        static const double negaziranSokTezina = 0.1;
+public:
+    static const double negaziranSokTezina = 0.1;
 
-        PaketSok(double volumenNaEdenPaket, int kolicina, bool gaziran)
-            : PaketPijalok(volumenNaEdenPaket, kolicina)
-        {
-            this->gaziran = gaziran;
-        }
+    PaketSok(double volumenNaEdenPaket, int kolicina, bool gaziran)
+        : PaketPijalok(volumenNaEdenPaket, kolicina)
+    {
+        this->gaziran = gaziran;
+    }
 
-        double vratiMasa()
-        {
-            return this->gaziran ? PaketPijalok::vratiMasa() : 
-                PaketPijalok::vratiMasa() + PaketSok::negaziranSokTezina * PaketPijalok::getKolicina();
-        }
+    double vratiMasa()
+    {
+        return this->gaziran ? PaketPijalok::vratiMasa() :
+            PaketPijalok::vratiMasa() + PaketSok::negaziranSokTezina * PaketPijalok::getKolicina();
+    }
 
-        void pecati()
-        {
-            cout << "Paket sok" << endl;
-            cout << "kolicina " << kolicina << ", so po " << 
-                 PaketPijalok::volumenNaEdenPaket * PaketPijalok::gustina << " l(dm3)" << endl; 
-        }
+    void pecati()
+    {
+        cout << "Paket sok" << endl;
+        cout << "kolicina " << kolicina << ", so po " <<
+            PaketPijalok::volumenNaEdenPaket * PaketPijalok::gustina << " l(dm3)" << endl;
+    }
 };
 
 
 class PaketVino : public PaketPijalok, public ImaMasa
 {
-    protected:
-        double procentNaAlkohol;
+protected:
+    double procentNaAlkohol;
 
-    public:
-        static constexpr double procentAlkoholKoeficient = 0.9;
+public:
+    static const double procentAlkoholKoeficient = 0.9;
 
-        PaketVino(double volumenNaEdenPaket, int kolicina, double procentNaAlkohol)
-            : PaketPijalok(volumenNaEdenPaket, kolicina)
+    PaketVino(double volumenNaEdenPaket, int kolicina, double procentNaAlkohol)
+        : PaketPijalok(volumenNaEdenPaket, kolicina)
+    {
+        try
         {
-            try
+            if (procentNaAlkohol > 1 || procentNaAlkohol < 0)
             {
-                if (procentNaAlkohol > 1 || procentNaAlkohol < 0)
-                {
-                    throw AlkoholPercentException("Pogresno vnesen procent na alkohol!");
-                }
-                this->procentNaAlkohol = procentNaAlkohol;
+                throw AlkoholPercentException();
             }
-            catch(AlkoholPercentException alkoholPercentException)
-            {
-                alkoholPercentException.printMessage();
-            }
-            
+            this->procentNaAlkohol = procentNaAlkohol;
+        }
+        catch (AlkoholPercentException alkoholPercentException)
+        {
+            alkoholPercentException.printMessage();
         }
 
-        double vratiMasa()
-        {
-            return PaketPijalok::vratiMasa() * (PaketVino::procentAlkoholKoeficient + 0.9);
-        }
+    }
 
-        void pecati()
-        {
-            cout << "Paket vino" << endl;
-            cout << "kolicina " << this->kolicina << ", " << this->procentNaAlkohol * 100 
-                 << "% alkohol od po " << this->volumenNaEdenPaket * PaketPijalok::gustina << " l(dm3)" << endl;
-        }      
+    double vratiMasa()
+    {
+        return PaketPijalok::vratiMasa() * (PaketVino::procentAlkoholKoeficient + this->procentNaAlkohol);
+    }
+
+    void pecati()
+    {
+        cout << "Paket vino" << endl;
+        cout << "kolicina " << this->kolicina << ", " << this->procentNaAlkohol * 100
+            << "% alkohol od po " << this->volumenNaEdenPaket * PaketPijalok::gustina << " l(dm3)" << endl;
+    }
 };
 
 
 class Kamion
 {
-    protected:
-        char *registracija;
-        char *vozac;
-        ImaMasa **paketi;
-        int brojNaPaketi;
+protected:
+    char registracija[9];
+    char vozac[50];
+    ImaMasa** paketi;
+    int brojNaPaketi;
 
-    public:
-        Kamion(char *registracija, char *vozac)
+public:
+    Kamion(char* registracija, char* vozac)
+    {
+        this->brojNaPaketi = 0;
+        strcpy(this->vozac, vozac);
+        this->registriraj(registracija);
+    }
+
+    void registriraj(char* registracija)
+    {
+        if (!isalpha(registracija[0]) || !isalpha(registracija[1]) ||
+            !isalpha(registracija[6]) || !isalpha(registracija[7]))
         {
-            this->brojNaPaketi = 0;
-            this->vozac = new char[strlen(vozac) + 1];
-            strcpy(this->vozac, vozac);
-            this->registriraj(registracija);
+            throw RegistrationException();
+        }
+        strcpy(this->registracija, registracija);
+    }
+
+    void dodadiElement(ImaMasa *paket)
+    {
+        ImaMasa **temp = new ImaMasa *[this->brojNaPaketi + 1];
+        for (int i = 0; i < this->brojNaPaketi; i++)
+        {
+            temp[i] = this->paketi[i];
         }
 
-        void registriraj(char *registracija)
-        {
-            if (!isalpha(registracija[0]) || !isalpha(registracija[1]) || 
-                !isalpha(registracija[6]) || !isalpha(registracija[7]))
-                {
-                    throw RegistrationException("Pogresno vnesena registracija!");
-                }
-            this->registracija = new char[strlen(registracija) + 1];
-            strcpy(this->registracija, registracija);
-        }
+        temp[this->brojNaPaketi] = paket;
+        this->brojNaPaketi++;
+        this->paketi = temp;
+    }
 
-        void dodadiElement(ImaMasa *paket)
+    void pecati()
+    {
+        cout << "Kamion so registracija " << this->registracija
+            << " i vozac " << this->vozac << " prenesuva: " << endl;
+        for (int i = 0; i < this->brojNaPaketi; i++)
         {
-            ImaMasa **temp = new ImaMasa*[this->brojNaPaketi + 1];
-            for (int i = 0; i < this->brojNaPaketi; i++)
+            this->paketi[i]->pecati();
+        }
+    }
+
+    Kamion pretovar(char* registracija, char* vozac)
+    {
+        Kamion novKamion(registracija, vozac);
+        double maxMasa = -1;
+        int maxMasaPozicija;
+        for (int i = 0; i < this->brojNaPaketi; i++)
+        {
+            if (this->paketi[i]->vratiMasa() > maxMasa)
             {
-                temp[i] = this->paketi[i];
+                maxMasaPozicija = i;
+                maxMasa = this->paketi[i]->vratiMasa();
             }
-
-            temp[this->brojNaPaketi] = paket;
-            this->brojNaPaketi++;
-
-            delete [] this->paketi;
-
-            this->paketi = temp;
         }
 
-        void pecati()
+        for (int i = 0; i < this->brojNaPaketi; i++)
         {
-            cout << "Kamion so registracija " << this->registracija
-                 << " i vozac " << this->vozac << " prenesuva:" << endl;
-            for (int i = 0; i < this->brojNaPaketi; i++)
+            if (i != maxMasaPozicija)
             {
-                this->paketi[i]->pecati();
+                novKamion.dodadiElement(this->paketi[i]);
             }
         }
 
-        Kamion pretovar(char *registracija, char *vozac)
+        return novKamion;
+    }
+
+    double vratiVkupnaMasa()
+    {
+        double vkupnaMasa = 0.0;
+
+        for (int i = 0; i < this->brojNaPaketi; i++)
         {
-            Kamion novKamion(registracija, vozac);
-            double maxMasa = -1;
-            int maxMasaPozicija;
-            for (int i = 0; i < this->brojNaPaketi; i++)
-            {
-                if (this->paketi[i]->vratiMasa() > maxMasa)
-                {
-                    maxMasaPozicija = i;
-                    maxMasa = this->paketi[i]->vratiMasa();
-                }
-            }
-
-            for (int i = 0; i < this->brojNaPaketi; i++)
-            {
-                if (i != maxMasaPozicija)
-                {
-                    novKamion.dodadiElement(this->paketi[i]);
-                }
-            }
-            
-            return novKamion;
+            vkupnaMasa += this->paketi[i]->vratiMasa();
         }
 
-        double vratiVkupnaMasa()
-        {
-            double vkupnaMasa = 0.0;
-
-            for (int i = 0; i < this->brojNaPaketi; i++)
-            {
-                vkupnaMasa += this->paketi[i]->vratiMasa();
-            }
-
-            return vkupnaMasa;
-        }
+        return vkupnaMasa;
+    }
 };
 
 
@@ -267,27 +243,27 @@ int main()
     int kol;
     bool g;
     double proc;
-    
+
     try
     {
-        cin>>reg;
-        cin>>ime;
+        cin >> reg;
+        cin >> ime;
         Kamion A(reg, ime);
-        ImaMasa **d = new ImaMasa*[5];
-        cin>>vol>>kol;
-        cin>>g;
+        ImaMasa** d = new ImaMasa * [5];
+        cin >> vol >> kol;
+        cin >> g;
         d[0] = new PaketSok(vol, kol, g);
-        cin>>vol>>kol;
-        cin>>proc;
+        cin >> vol >> kol;
+        cin >> proc;
         d[1] = new PaketVino(vol, kol, proc);
-        cin>>vol>>kol;
-        cin>>proc;
+        cin >> vol >> kol;
+        cin >> proc;
         d[2] = new PaketVino(vol, kol, proc);
-        cin>>vol>>kol;
-        cin>>g;
+        cin >> vol >> kol;
+        cin >> g;
         d[3] = new PaketSok(vol, kol, g);
-        cin>>vol>>kol;
-        cin>>proc;
+        cin >> vol >> kol;
+        cin >> proc;
         d[4] = new PaketVino(vol, kol, proc);
 
         A.dodadiElement(d[0]);
@@ -296,15 +272,15 @@ int main()
         A.dodadiElement(d[3]);
         A.dodadiElement(d[4]);
         A.pecati();
-        cout<<"Vkupna masa: "<<A.vratiVkupnaMasa()<<endl;
-        cin>>reg;
-        cin>>ime;
+        cout << "Vkupna masa: " << A.vratiVkupnaMasa() << endl;
+        cin >> reg;
+        cin >> ime;
         Kamion B = A.pretovar(reg, ime);
         B.pecati();
-        cout<<"Vkupna masa: "<<B.vratiVkupnaMasa()<<endl;
+        cout << "Vkupna masa: " << B.vratiVkupnaMasa() << endl;
 
     }
-    catch(RegistrationException registrationException)
+    catch (RegistrationException registrationException)
     {
         registrationException.printMessage();
     }
